a1bbc9907f6009f5e7e55f35e7f863131913b2b5
karishmaeceskec <121019069+karishmaeceskec@users.noreply.github.com>
2022-12-22 13:04:59 +0530

    Update models.py

--- MyApps1/models.py
+++ MyApps1/models.py
@@ -16,6 +16,6 @@ from django.db import models
 #Create your models here.
 class Employee(models.Model):
     eno=models.IntegerField()
-    ename=models.CharField(max_length=64)
+    ename=models.CharField(max_length=256)
     esal=models.FloatField()
     eaddr=models.CharField(max_length=256)
